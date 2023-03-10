039b8ebc0c03141910e709d1644fb25db5505977
Heeesuu <gmltn0508@icloud.com>
2023-03-10 17:42:32 +0900

    여러수 더하기

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -5,27 +5,13 @@ public class Calc {
         exp = exp.replaceAll("- ","+ -" );
 
         String[] bits = exp.split(" \\+ ");
+        int sum = 0;
 
-        int a = Integer.parseInt(bits[0]);
-        int b = Integer.parseInt(bits[1]);
-        int c = 0;
-        int d = 0;
+        for (int i = 0; i < bits.length; i++) {
+            sum += Integer.parseInt(bits[i]);
 
-        if (bits.length > 3 ) { // 쪼갯을때 길이가 3보다 크다
-            c = Integer.parseInt(bits[2]);
-            d = Integer.parseInt(bits[3]);
-            return a + b + c + d;
         }
-
-        if (bits.length > 2 ) { // 쪼갯을때 길이가 2보다 크다
-                c = Integer.parseInt(bits[2]);
-                return a + b + c;
-        }
-
-
-            return a + b;
-
-
+        return sum;
     }
 }
 
